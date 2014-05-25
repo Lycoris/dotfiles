f094edf9422e9d6e7e416e667290024c291283ee
Lycoris_i <some.chicken@gmail.com>
2014-05-25 18:20:51 +0900

    gitのエイリアス系をすべて削除

--- .zshrc.orig.zsh
+++ .zshrc.orig.zsh
@@ -3,9 +3,9 @@ alias ls='ls -G'
 alias la='ls -aG'
 alias grep='grep -G'
 alias ll='ls -lG'
-alias vi='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
-alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
-alias emacs='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
+alias vi='/Applications/MacVim.app/Contents/MacOS/Vim "$@"'
+alias vim='/Applications/MacVim.app/Contents/MacOS/Vim "$@"'
+alias emacs='/Applications/MacVim.app/Contents/MacOS/Vim "$@"'
 alias tv='tmux -v'
 alias tmux='tmux -2 -u'
 alias tnew='tmux new -s'
@@ -22,15 +22,6 @@ alias dp='dvipdfmx'
 alias lmk='latexmk -dvi -f'
 alias ndl='ruby niconicodl -s -m'
 
-#git
-alias gadd='git add'
-alias gremote='git remote'
-alias gst='git status -uno'
-alias gcommit='git commit -m'
-alias gpush='git push'
-alias gpull='git pull'
-alias gclone='glt clone'
-
 #vim関連
 alias wp='vim +":BlogNew"'
 
@@ -40,10 +31,10 @@ export PATH="/usr/local/texlive/2013/bin/x86_64-linux:$PATH"
 export TERM=xterm-256color
 export LANG=ja_JP.UTF-8
 export EDITOR="vim"
-export PATH="$HOME/.rbenv/bin:$PATH"
+#export PATH="$HOME/.rbenv/bin:$PATH"
 export INFOPATH="/usr/local/texlive/2013/texmf-dist/doc/info"
 export MANPATH="/usr/local/texlive/2013/texmf-dist/doc/man"
-eval "$(rbenv init -)"
+#eval "$(rbenv init -)"
 
 #if [ -z $TMUX ]; then
 #  if $(tmux has-session); then
@@ -62,7 +53,7 @@ compinit
 autoload -Uz colors
 colors
 
-function chpwd() { ls --color=auto }
+function chpwd() { ls -G }
 HISTFILE=~/.zsh_histfile
 HISTSIZE=10000
 SAVEHIST=10000
