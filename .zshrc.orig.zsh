493147b2f747e151d26f963f37ae16ee8e290c78
Lycoris_i <some.chicken@gmail.com>
2014-06-11 14:00:06 +0900

    Powerline導入だけ反映

--- .zshrc.orig.zsh
+++ .zshrc.orig.zsh
@@ -19,7 +19,6 @@ alias pl='platex -kanji=utf8 -interaction=nonstopmode'
 alias dvipdfmx='/usr/local/bin/dvipdfmx'
 alias dp='dvipdfmx'
 alias lmk='latexmk -dvi -f'
-alias ndl='ruby niconicodl -s -m'
 
 #git
 alias gadd='git add'
